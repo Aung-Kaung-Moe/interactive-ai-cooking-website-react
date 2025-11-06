cd6520f8012168d44b272d932a4899d07f420023
Aung-Kaung-Moe <aungkaungmoe@uit.edu.mm>
2025-11-06 22:17:52 +0630

    Create Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import chefClaudeLogo from "./images/chef-claude-icon.png"
+
+export default function Header() {
+    return (
+        <header>
+            <img src={chefClaudeLogo}/>
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
